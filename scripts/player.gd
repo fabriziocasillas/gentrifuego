4ccc1328dec78da83da1e5000216972bd1a26f4e
fabriziopimentel <fabriziopimentel@ciencias.unam.mx>
2025-06-30 15:09:17 -0600

    :sparkles: added the player, the movement and players life

---
+++ scripts/player.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+var health = 3.0
+signal health_depleted
+
+
+func _physics_process(delta: float) -> void:
+	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	const DAMAGE_RATE = 0.5
+	velocity = direction * 700
+
+	move_and_slide()
+
+
+		
+	var overlapping_bodies = $HurtBox.get_overlapping_bodies()
+	if overlapping_bodies.size() > 0:
+		health -= DAMAGE_RATE * overlapping_bodies.size() * delta
+		%ProgressBar.value = health
+		%ProgressBar.max_value = 100.0
+
+		if health <= 0.0:
+			health_depleted.emit()
+			
